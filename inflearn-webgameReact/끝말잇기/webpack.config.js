6df594861b1a886b0ae36b3af2fd493bf5b09085
xeropise <50399804+xeropise@users.noreply.github.com>
2021-02-19 22:58:19 +0800

    Add files via upload

---
+++ inflearn-webgameReact/끝말잇기/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path"); // NodeJs의 경로 조작 기능 가져옴
+
+module.exports = {
+  name: "word-relay-setting", //웹팩 설정 이름
+  mode: "development", // 실서비스: production
+  devtool: "eval", // 빠르게?
+  resolve: {
+    extensions: [".js", ".jsx"], // 이 확장자로 끝나는 걸 읽어 와
+  },
+  entry: {
+    app: ["./client"], // client.jsx 가 이미 WordRelay.jsx 를 불러오고 있으므로, 웹팩이 알아서 묶어 준다.
+  }, // 입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"), // 폴더를 dist 로
+    filename: "app.js",
+  }, // 출력
+};
