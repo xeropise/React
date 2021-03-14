054476fec3655f15a64aa8681c3fa99f5aeed289
xeropise <whrbql1@gmail.com>
2021-03-14 22:44:04 +0900

    second commit(3)

---
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
+  );
+
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
+  //   dispatch,
+  // ]);
+
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default TodosContainer;
