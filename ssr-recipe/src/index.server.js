03ab03ad2da496302a6777ea644f679197394395
xeropise <whrbql1@gmail.com>
2021-03-17 21:44:49 +0900

    17th Marh commit

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -5,6 +5,11 @@ import { StaticRouter } from "react-router-dom";
 import App from "./App";
 import path from "path";
 import fs from "fs";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./modules";
+import PreloadContext from "./lib/PreloadContext";
 
 // asset-manifest.json에서 파일 경로들을 조회하자.
 const manifest = JSON.parse(
@@ -50,11 +55,30 @@ const serverRender = (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
   const context = {};
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+
+  const preloadContext = {
+    done: false,
+    promises: [],
+  };
+
   const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
   );
+
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한 번 렌더링한다.
+  try {
+     await Promise.all(preloadContext.promises); // 모든 프로미스를 기다린다.
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
   res.send(createPage(root)); // 클라이언트에게 결과물을 응답합니다.
 };
