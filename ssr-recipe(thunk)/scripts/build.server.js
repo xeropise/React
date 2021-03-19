eeb13ed2394d544c07c2e79152348055fd62866e
xeropise <whrbql1@gmail.com>
2021-03-19 22:43:47 +0900

    19th Mar

--- ssr-recipe(thunk)/scripts/build.server.js
+++ ssr-recipe(thunk)/scripts/build.server.js
@@ -8,20 +8,20 @@ process.on("unhandledRejection", (err) => {
 require("../config/env");
 const fs = require("fs-extra");
 const webpack = require("webpack");
-const config = require("../config/webpack.config.server.js");
+const config = require("../config/webpack.config.server");
 const paths = require("../config/paths");
 
 function build() {
-  console.log("Create server build...");
+  console.log("Creating server build...");
   fs.emptyDirSync(paths.ssrBuild);
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
-    compiler.run((err, status) => {
+    compiler.run((err, stats) => {
       if (err) {
         console.log(err);
         return;
       }
-      console.log(status.toString());
+      console.log(stats.toString());
     });
   });
 }
