bdd3dc8f2f99f155de0439480e5fc67edd66e30e
xeropise <whrbql1@gmail.com>
2021-03-19 23:54:58 +0900

    19th march

--- ssr-recipe(thunk)/src/index.js
+++ ssr-recipe(thunk)/src/index.js
@@ -7,25 +7,42 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "./modules";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+import { loadableReady } from "@loadable/component";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__, // 이 값을 초기상태로 사용함
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
 
-ReactDOM.render(
-  <React.StrictMode>
+sagaMiddleware.run(rootSaga);
+
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+  return (
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-    ,
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  );
+};
+
+const root = document.getElementById("root");
+
+// 프로덕선 환경에서는 loadableReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+if (process.env.NODE_ENV === "production") {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root);
+  });
+} else {
+  ReactDOM.render(<Root />, root);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- ssr-recipe(thunk)/src/pages/UsersPage.js
+++ ssr-recipe(thunk)/src/pages/UsersPage.js
@@ -1,8 +1,18 @@
 import React from "react";
-import UsersContainer from "../containers/UsersContainer";
+import UsersContainer from "../containers/UserContainer";
+import UserContainer from "../containers/userContainer";
+import { Route } from "react-router-dom";
 
 const UsersPage = () => {
-  return <UsersContainer />;
+  return (
+    <>
+      <UsersContainer />;
+      <Route
+        path="/users/:id"
+        render={({ match }) => <UserContainer id={match.params.id} />}
+      />
+    </>
+  );
 };
 
 export default UsersPage;
