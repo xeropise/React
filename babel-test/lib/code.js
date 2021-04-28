5b2085c684bc87e6c2afe3af592355c4a2de8b23
xeropise <whrbql1@gmail.com>
2021-04-28 23:52:08 +0900

    28th Apr

--- babel-test/lib/code.js
+++ babel-test/lib/code.js
@@ -1,9 +1,11 @@
+"use strict";
+
 // cod.ejs
 const p = Promise.resolve(10);
 const obj = {
   a: 10,
   b: 20,
-  c: 30,
+  c: 30
 };
 const arr = Object.values(obj);
 const eixst = arr.includes(20);
